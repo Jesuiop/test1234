b80308a8ef13297e3035e272e41d00c54fed4b51
John Doe <johndoe@example.com>
2023-03-06 13:19:04 +0100

    Revert "metido condicional"
    This reverts commit 176697d6c2bd2fb331322e74d366b58352141cb6.

--- ej1.py
+++ ej1.py
@@ -6,13 +6,8 @@ comentario
 de nose
 muchas lineas
 '''
-valor=8
 def hola(nombre_recibido):
     
-    if valor < 4:
-        print("Hola")
-    elif valor >=4 and valor < 9:
-        print("Adios")
+    
     print("Hola mundo " + nombre_recibido)
-    return 1
 hola(nombre)
